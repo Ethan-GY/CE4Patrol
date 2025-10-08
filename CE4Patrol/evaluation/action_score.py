49f04fbba31d06ae8c6d3bcb4a4c85d68fa39da2
GaoE <3490352665@qq.com>
2025-10-08 22:28:39 +0800

    adjust project structure

--- CE4Patrol/evaluation/action_score.py
+++
@@ -1,6 +0,0 @@
-def action_score(predicted_actions, ground_actions):
-    set_pred = set(predicted_actions)
-    set_true = set(ground_actions)
-    intersection = len(set_pred & set_true)
-    union = len(set_pred | set_true)
-    return intersection / union if union > 0 else 0.0
